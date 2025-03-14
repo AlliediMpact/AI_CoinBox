4ad3ec74e1535db911a8e7847b1b9e0d2fad0859
Jacky Mngomezulu <jackymngomezulu@gmail.com>
2025-03-14 03:07:42 +0200

    Initial commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,36 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'services/notification_service.dart';
-import 'screens/auth/login_screen.dart';
+import 'my_app.dart'; // Make sure this file exists in the lib/ directory
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
-  await NotificationService().initialize();
   runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
-  /// Builds a banking-style theme.
-  ThemeData _buildTheme() {
-    return ThemeData(
-      brightness: Brightness.light,
-      primarySwatch: Colors.blue,
-      appBarTheme: const AppBarTheme(backgroundColor: Colors.blueAccent),
-      elevatedButtonTheme: ElevatedButtonThemeData(
-        style: ElevatedButton.styleFrom(backgroundColor: Colors.blueAccent),
-      ),
-    );
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Allied iMpact Coin Box',
-      theme: _buildTheme(),
-      home: const LoginScreen(),
-    );
-  }
 }
